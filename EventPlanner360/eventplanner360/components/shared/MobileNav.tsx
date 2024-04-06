1b1e866f7e4d75d9b6a3ded003989d31fa67b0fe
Houdini Papi <ochiengcliff.co@gmail.com>
2024-04-06 15:53:39 +0300

    Update MobileNav component with menu icon and logo

--- EventPlanner360/eventplanner360/components/shared/MobileNav.tsx
+++ EventPlanner360/eventplanner360/components/shared/MobileNav.tsx
@@ -6,20 +6,28 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import Image from 'next/image'
 
 const MobileNav = () => {
   return (
     <nav className="md:hidden">
         <Sheet>
-            <SheetTrigger>Open</SheetTrigger>
-            <SheetContent>
-                <SheetHeader>
-                    <SheetTitle>Are you absolutely sure?</SheetTitle>
-                        <SheetDescription>
-                            This action cannot be undone. This will permanently delete your account
-                            and remove your data from our servers.
-                        </SheetDescription>
-                </SheetHeader>
+            <SheetTrigger className="align-middle">
+                <Image
+                    src="/assets/icons/menu.svg"
+                    width={24}
+                    height={24}
+                    alt="menu"
+                    className="cursor-pointer"
+                />
+            </SheetTrigger>
+            <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+                <Image
+                    src="/assets/images/logo.svg"
+                    alt="logo"
+                    width={128}
+                    height={38}
+                />
             </SheetContent>
         </Sheet>
     </nav>
