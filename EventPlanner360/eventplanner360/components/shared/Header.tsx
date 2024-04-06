3a4785bec44c7952f2cf4cd9ee713a610b0df8d6
Houdini Papi <ochiengcliff.co@gmail.com>
2024-04-06 15:37:30 +0300

    Add NavItems component to Header.tsx and create NavItems.tsx

--- EventPlanner360/eventplanner360/components/shared/Header.tsx
+++ EventPlanner360/eventplanner360/components/shared/Header.tsx
@@ -2,6 +2,7 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
 
 
 const Header = () => {
@@ -14,9 +15,16 @@ const Header = () => {
                 />
             </Link>
 
+            <SignedIn>
+                <nav className="md:flex-between hidden w-full max-w-xs">
+                    <NavItems />
+                </nav>
+            </SignedIn>
+
             <div className='flex w-32 justify-end gap-3'>
                 <SignedIn>
                     <UserButton afterSignOutUrl="/" />
+                    <NavItems />
                 </SignedIn>
 
 
